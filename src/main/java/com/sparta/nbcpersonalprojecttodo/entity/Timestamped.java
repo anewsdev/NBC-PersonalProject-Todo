faaffc298a98222a2102547a4f03f3224d167f26
anewsdev <anewsdev@gmail.com>
2024-10-15 20:28:56 +0900

    Timestamp 추가

---
+++ src/main/java/com/sparta/nbcpersonalprojecttodo/entity/Timestamped.java
@@ -0,0 +1,25 @@
+package com.sparta.nbcpersonalprojecttodo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+
+    @CreatedDate
+    @Column(updatable = false) //업데이트가 되지않게 막음
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate //조회한 entity 값을 변경할때 변경시간이 자동으로 적용
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}
