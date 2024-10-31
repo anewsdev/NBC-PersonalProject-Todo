de3dded0518a0445520b75b47d83a9341e18befd
anewsdev <anewsdev@gmail.com>
2024-10-31 12:34:59 +0900

    유효성검사 추가

--- src/main/java/com/sparta/nbcpersonalprojecttodo/user/entity/User.java
+++ src/main/java/com/sparta/nbcpersonalprojecttodo/user/entity/User.java
@@ -4,6 +4,8 @@ import com.sparta.nbcpersonalprojecttodo.user.dto.UserRequestDto;
 import com.sparta.nbcpersonalprojecttodo.common.entity.Timestamped;
 import com.sparta.nbcpersonalprojecttodo.userTodo.entity.UserTodo;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,6 +26,8 @@ public class User extends Timestamped {
     @Column(name = "username", nullable = false)
     private String username;
 
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Column(name = "email", nullable = false)
     private String email;
 
